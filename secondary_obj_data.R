48d52d004d90f6272f0a9b551d7ed6aaf10d79eb
mbi6245 <mbi6245@uw.edu>
2025-02-02 13:59:56 -0800

    created but did not save hypertension patients with pre and post readings, also not yet filtered on race

--- secondary_obj_data.R
+++ secondary_obj_data.R
@@ -7,3 +7,37 @@ library(flextable)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 fp_gdp = file.path(getwd(), "general_data_prep.R")
 source(fp_gdp)
+
+first_koh_date = ymd("2023-04-05")
+
+# subset of patient ids w/hypertension
+htn_ids = koh.table1 %>%
+  filter(HTN == 1) %>%
+  select("UniqueIdentifier")
+
+# htn ptids and all BP readings/associated dates ONLY no other vars
+all_htn_reads = left_join(htn_ids, bp.nona.18, by="UniqueIdentifier") %>%
+  select(-age)
+
+# set of htn ptids with readings before first KOH meeting
+pre_htn_read_ids = all_htn_reads %>%
+  filter(Date <= first_koh_date)
+
+pre_htn_read_ids = pre_htn_read_ids$UniqueIdentifier %>%
+  as.array() %>% 
+  unique()
+
+# set of htn ptids with readings after first KOH meeting
+post_htn_read_ids = all_htn_reads %>% 
+  filter(Date > first_koh_date)
+
+post_htn_read_ids = post_htn_read_ids$UniqueIdentifier %>% 
+  as.array() %>% 
+  unique()
+
+# intersection of above sets (yields ptids with readings before and after first KOH meeting)
+pre_post_ptids = intersect(pre_htn_read_ids, post_htn_read_ids)
+
+# filter for only patients in above intersection
+htn_reads_LME = all_htn_reads %>% 
+  filter(UniqueIdentifier %in% pre_post_ptids)
