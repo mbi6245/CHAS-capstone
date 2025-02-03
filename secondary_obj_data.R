0db0653bfe9b42422500b0ac652056bca409082e
mbi6245 <mbi6245@uw.edu>
2025-02-02 17:44:42 -0800

    seconday objective analysis dataset creation code

--- secondary_obj_data.R
+++ secondary_obj_data.R
@@ -10,9 +10,13 @@ source(fp_gdp)
 
 first_koh_date = ymd("2023-04-05")
 
-# subset of patient ids w/hypertension
-htn_ids = koh.table1 %>%
-  filter(HTN == 1) %>%
+##################################
+# HYPERTENSION DATASET PREPARATION
+##################################
+
+# subset of Marshallese and NHW patient ids w/hypertension
+htn_ids = table1 %>%
+  filter(HTN == 1 & (Marsh == 1 | Race == "White")) %>%
   select("UniqueIdentifier")
 
 # htn ptids and all BP readings/associated dates ONLY no other vars
@@ -41,3 +45,59 @@ pre_post_ptids = intersect(pre_htn_read_ids, post_htn_read_ids)
 # filter for only patients in above intersection
 htn_reads_LME = all_htn_reads %>% 
   filter(UniqueIdentifier %in% pre_post_ptids)
+
+write.csv(htn_reads_LME, "Analysis Data/Obj2BP_LME.csv")
+
+# drop all measurements not taken at earliest and most recent dates
+htn_reads_pp = htn_reads_LME %>% 
+  group_by(UniqueIdentifier) %>% 
+  filter(Date == min(Date) | Date == max(Date)) %>% 
+  ungroup()
+
+write.csv(htn_reads_pp, "Analysis Data/Obj2BPPrePost.csv")
+
+##########################
+# T2DM DATASET PREPARATION
+##########################
+
+# subset of Marshallese and NHW patient ids w/hypertension
+a1c_ids = table1 %>%
+  filter(Diabetes == 1 & (Marsh == 1 | Race == "White")) %>%
+  select("UniqueIdentifier")
+
+# htn ptids and all BP readings/associated dates ONLY no other vars
+all_a1c_reads = left_join(a1c_ids, a1c.nona.18, by="UniqueIdentifier") %>%
+  select(-age)
+
+# set of htn ptids with readings before first KOH meeting
+pre_a1c_read_ids = all_a1c_reads %>%
+  filter(Date <= first_koh_date)
+
+pre_a1c_read_ids = pre_a1c_read_ids$UniqueIdentifier %>%
+  as.array() %>% 
+  unique()
+
+# set of htn ptids with readings after first KOH meeting
+post_a1c_read_ids = all_a1c_reads %>% 
+  filter(Date > first_koh_date)
+
+post_a1c_read_ids = post_a1c_read_ids$UniqueIdentifier %>% 
+  as.array() %>% 
+  unique()
+
+# intersection of above sets (yields ptids with readings before and after first KOH meeting)
+pre_post_ptids = intersect(pre_a1c_read_ids, post_a1c_read_ids)
+
+# filter for only patients in above intersection
+a1c_reads_LME = all_a1c_reads %>% 
+  filter(UniqueIdentifier %in% pre_post_ptids)
+
+write.csv(a1c_reads_LME, "Analysis Data/Obj2A1c_LME.csv")
+
+# drop all measurements not taken at earliest and most recent dates
+a1c_reads_pp = a1c_reads_LME %>% 
+  group_by(UniqueIdentifier) %>% 
+  filter(Date == min(Date) | Date == max(Date)) %>% 
+  ungroup()
+
+write.csv(a1c_reads_pp, "Analysis Data/Obj2A1cPrePost.csv")
